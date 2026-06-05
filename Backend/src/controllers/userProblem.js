6aa82daf494a84d0b44413cdafb0b50bed53d9ea
ankit <rathaurankit501@gmail.com>
2026-06-05 17:01:46 +0530

    created submitToken method

---
+++ Backend/src/controllers/userProblem.js
@@ -0,0 +1,43 @@
+const { Problem } = require("../models/problem");
+const {
+  getLanguageById,
+  submitBatch,
+  submitToken,
+} = require("../utils/problemUtility");
+
+const createProblem = async (req, res) => {
+  const {
+    title,
+    description,
+    difficulty,
+    tags,
+    visibleTestCases,
+    hiddenTestCases,
+    referenceSolution,
+    startCode,
+    problemCreator,
+  } = req.body;
+  try {
+    for (const { language, completeCode } of referenceSolution) {
+      const languageId = getLanguageById(language);
+      const submissions = visibleTestCases.map((testcase) => ({
+        source_code: completeCode,
+        language_id: languageId,
+        stdin: testcase.input,
+        expected_output: testcase.output,
+      }));
+
+      const submitResult = await submitBatch(submissions);
+      const resultToken = submitResult.map((value) => value.token);
+      const testResult = await submitToken(resultToken);
+
+      for (const test of testResult) {
+        if (test.status_id != 3) return res.status(400).send("Error Occured");
+      }
+    }
+    const userProblem = await problem.create({
+      ...req.body,
+      problemCreator: req.result._id,
+    });
+  } catch (err) {}
+};
